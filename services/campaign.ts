f20f9339100e050495865f213871a3a74b226afd
Parminder Singh <parmindersingh.swe@gmail.com>
2024-04-23 20:02:12 -0300

    Integrated the firebase with the Campaign form

---
+++ services/campaign.ts
@@ -0,0 +1,33 @@
+import db from "../configurations/firebase";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { CampaignCustomizationModel } from "@/Interfaces/Campaign/CampaignCustomizationModel";
+
+export const addCampaign = async (campaignData: any) => {
+  try {
+    const docRef = await addDoc(collection(db, "Campaign"), campaignData);
+    console.log("Document written with ID: ", docRef.id);
+    return docRef;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+export const getCampaignCustomizations = async (): Promise<
+  CampaignCustomizationModel[]
+> => {
+  const campaignsCol = collection(db, "CampaignCustomization");
+  const campaignsSnapshot = await getDocs(campaignsCol);
+  const campaignsList = campaignsSnapshot.docs.map((doc) => {
+    const data = doc.data();
+    return {
+      Banner: data.Banner,
+      Name: data.Name,
+      CompanyName: data.CompanyName,
+      ContactNumber: data.ContactNumber,
+      CompanyEmailId: data.CompanyEmailId,
+      Comments: data.Comments,
+      CTA: data.CTA,
+    };
+  });
+  return campaignsList;
+};
