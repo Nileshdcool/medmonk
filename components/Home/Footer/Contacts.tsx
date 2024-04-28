05831ee8ea8f4f0ae804176e6989b54218821361
nilesh-sukalikar <neil.sukalikar@gmail.com>
2024-04-28 19:41:35 +0530

    fixes

--- components/Home/Footer/Contacts.tsx
+++ components/Home/Footer/Contacts.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { ContactsList } from "@/Constants/HomePage/Footer/contactsList";
+import { ContactsList } from "@/Constants/HomePage/Footer/ContactsList";
 const Contacts = () => {
   return (
     <div className="w-full">
