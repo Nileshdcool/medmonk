68654e49f9cf7260be193d4b6e1b2001722a4daa
Parminder Singh <parmindersingh.swe@gmail.com>
2024-04-29 14:16:02 -0300

    show the hidden text and hides the dummy accredited by

--- Constants/HomePage/Footer/AccreditedByList.ts
+++ Constants/HomePage/Footer/AccreditedByList.ts
@@ -1,6 +1,6 @@
 import { AccreditedByType } from "@/Interfaces/Home/Footer/AccreditedByType";
 
 export const AccreditedByList: AccreditedByType[] = [
-  { text: "Text", logo: "Logo", href: "#" },
-  { text: "Text", logo: "Logo", href: "#" },
+  // { text: "Text", logo: "Logo", href: "#" },
+  // { text: "Text", logo: "Logo", href: "#" },
 ];
