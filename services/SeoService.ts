b4eba0270c8b2a538551ee52972ba956e5ce80ab
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-02 10:02:00 -0300

    Removed the unnecessary comments.

--- services/SeoService.ts
+++ services/SeoService.ts
@@ -1,5 +1,5 @@
-import { MetaProps } from "@/Interfaces/SEO/MetaProps";
 import db from "../configurations/firebase";
+import { MetaProps } from "@/Interfaces/SEO/MetaProps";
 import {
   DocumentSnapshot,
   DocumentData,
@@ -7,16 +7,18 @@ import {
   getDoc,
 } from "firebase/firestore";
 
-export const getMetaTags = async (pageId: string): Promise<MetaProps | null> => {
+export const getMetaTags = async (
+  pageId: string
+): Promise<MetaProps | null> => {
   try {
     const pageRef = doc(db, "SEO", pageId);
     const pageDoc: DocumentSnapshot<DocumentData> = await getDoc(pageRef);
 
     if (pageDoc.exists()) {
-      const pageData = pageDoc.data() as MetaTags;  // Ensure MetaTags is correctly defined
-      return { metaData: pageData };  // Ensure MetaProps expects an object with metaData property
+      const pageData = pageDoc.data() as MetaTags;
+      return { metaData: pageData };
     } else {
-      console.log("Page not found.: "+ pageId);
+      console.log("Page not found.: " + pageId);
       return null;
     }
   } catch (error) {
