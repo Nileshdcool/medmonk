95d09b1a8fdafab1f2b58ea3ea3917d55f1b4c75
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-09 15:51:31 -0300

    Added content in Privacy policy, Legal statement, and Cookies pages.

---
+++ Interfaces/PrivacyPolicy/PrivacyPolicyType.ts
@@ -0,0 +1,12 @@
+export interface PrivacyPolicyType {
+  Title: string;
+  Introduction: string;
+  InformationWeCollect: string;
+  HowWeUseInformation: string;
+  Cookies: string;
+  ThirdPartyDisclosure: string;
+  SecurityMeasures: string;
+  ChangesToThePolicy: string;
+  ContactUs: string;
+  ContactEmail: string;
+}
---
+++ Interfaces/TermsAndConditions/TermsAndConditionsType.ts
@@ -0,0 +1,11 @@
+export interface TermsAndConditionsType {
+  Title: string;
+  Introduction: string;
+  IntellectualProperty: string;
+  UseOfWebsiteAndServices: string;
+  MedicalInformation: string;
+  LimitationOfLiability: string;
+  ChangesToPolicy: string;
+  ContactInfo: string;
+  ContactEmail: string;
+}
