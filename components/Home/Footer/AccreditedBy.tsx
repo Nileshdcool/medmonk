7e12b26caf6ccb449f83edbd9e880bf7a95ebc64
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-10 14:20:25 -0300

    Added logo under the Accredited By in the footer.

--- components/Home/Footer/AccreditedBy.tsx
+++ components/Home/Footer/AccreditedBy.tsx
@@ -1,19 +1,20 @@
-import { AccreditedByList } from "@/Constants/HomePage/Footer/AccreditedByList";
+import Image from 'next/image';
 
 const AccreditedBy = () => {
   return (
-    <div className=" w-1/2 border-l-2 md:border-l-0 border-darkBlue pl-8 md:pl-0">
-      <h3 className="font-semibold text-hBlack mb-2 text-lg">Accredited by</h3>
-      <ul className="flex flex-col gap-1">
-        {AccreditedByList.map((item, index) => (
-          <li key={index}>
-            <a href={item.href} className="hover:text-brownCust">
-              {item.text}/{item.logo}?
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div className="w-1/4 flex flex-col items-center justify-center">
+      <h3 className="font-semibold text-gray-800 mb-3 text-lg whitespace-nowrap">Accredited by</h3>
+      <div className="relative w-full h-24 flex justify-center items-center">
+        <Image
+          src="/assets/AccreditedBy.jpg"
+          alt="Accreditation Logo"
+          layout="responsive"
+          width={100}
+          height={60}
+        />
+      </div>
     </div>
   );
 };
+
 export default AccreditedBy;
