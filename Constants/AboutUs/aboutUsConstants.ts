e5ec8667f2e249e99923effc4d4d720559b22ed2
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-20 21:01:42 -0300

    Refactor the code of about us page

---
+++ Constants/AboutUs/aboutUsConstants.ts
@@ -0,0 +1,118 @@
+const asATeamParagraphs: string[] = [
+  "At Medmonk, we take pride in our long history of patient advocacy. From the very beginning, our focus has always been clear: patients are our top priority.",
+  "Medmonks story began in 2009 when co-founder Somaira Punjwani, an HIV specialty pharmacist, developed a unique process to help patients struggling with their co-pays instead of turning them away. For over three years, she managed this manually, receiving tremendous positive feedback. This innovative approach led to the founding of Medmonk in 2012. As a Silicon Valley-based company incubated by Y Combinator, we have leveraged our innovative roots to build a robust and impactful patient support system.",
+  "Our team is composed of dedicated pharmacists, nurses, patient advocates, and reimbursement specialists who bring extensive experience and a shared passion for supporting patients with their life-saving medications. We understand the unique challenges faced by patients with rare diseases and are committed to going the extra mile to assist them. This dedication is reflected in Medmonks impressive average brand retention of seven years.",
+  "At Medmonk, patient care is not just a commitment—its our mission. Join us in making a difference in patients lives.",
+];
+const aboutLeadership = `At Medmonk, were more than just a company; were a passionate team
+              of professionals committed to making a meaningful impact in the
+              lives of patients. Our founding members, comprised of pharmacists
+              and technologists, bring decades of collective experience in
+              patient care and support.`;
+const teamMembers: TeamMember[] = [
+  {
+    name: "ASHAR HASAN R.",
+    title: "Chief Executive Officer",
+    image: "/assets/team-member.jpg",
+    degree: "Ph, MBA",
+    position: "Chief Executive Officer",
+    description:
+      "Transitioned from Chief Business Development Officer to Chief Executive Officer, bringing over a decade of experience in strategic innovation and business growth at Medmonk...",
+    descriptionParagraphs: [
+      "Transitioned from Chief Business Development Officer to Chief Executive Officer, bringing over a decade of experience in strategic innovation and business growth at Medmonk. Passionate about revolutionizing access and affordability, Ashar leads Medmonk, a technology-driven company specializing in patient support services for the treatment of complex chronic rare diseases. As a dedicated pharmacist, Ashar consistently places patients first, ensuring that all strategies and initiatives enhance both patient and provider experiences.",
+      "As the visionary founder and CEO of Premier Specialty Infusion (PSI), Ashar transformed the specialty infusion services landscape nationwide. Previously, co-founded and led Envoy Innovations, empowering healthcare providers with cutting-edge technology to analyze treatment outcomes. At Walgreens/OptionCare, led the Immunoglobulin Program to national prominence following the acquisition of Rx Solutions, where Ashar served as CEO/President and guided it into becoming the fastest-growing specialty injectable provider. Early leadership roles at Crescent Healthcare and Apria Healthcare provided a strong foundation in operational and strategic expertise.",
+      "In addition to leadership roles, Ashar has authored numerous publications related to treatment modalities for complex chronic therapies, further contributing to the field. Combining visionary thinking and industry expertise, Ashar consistently delivers exceptional results and advances specialty drug therapy delivery, always with a patient-first approach.",
+    ],
+  },
+  {
+    name: "ABBAS MEHDI",
+    title: "Chief Product Officer",
+    image: "/assets/team-member.jpg",
+    degree: "BASc",
+    position: "Chief Product Officer",
+    description:
+      "Abbas Mehdi recently transitioned to the role of Chief Product Officer after serving as CEO of Medmonk for eleven years. Under his visionary leadership...",
+    descriptionParagraphs: [
+      "Transitioned from Chief Business Development Officer to Chief Executive Officer, bringing over a decade of experience in strategic innovation and business growth at Medmonk. Passionate about revolutionizing access and affordability, Ashar leads Medmonk, a technology-driven company specializing in patient support services for the treatment of complex chronic rare diseases. As a dedicated pharmacist, Ashar consistently places patients first, ensuring that all strategies and initiatives enhance both patient and provider experiences.",
+      "As the visionary founder and CEO of Premier Specialty Infusion (PSI), Ashar transformed the specialty infusion services landscape nationwide. Previously, co-founded and led Envoy Innovations, empowering healthcare providers with cutting-edge technology to analyze treatment outcomes. At Walgreens/OptionCare, led the Immunoglobulin Program to national prominence following the acquisition of Rx Solutions, where Ashar served as CEO/President and guided it into becoming the fastest-growing specialty injectable provider. Early leadership roles at Crescent Healthcare and Apria Healthcare provided a strong foundation in operational and strategic expertise.",
+      "In addition to leadership roles, Ashar has authored numerous publications related to treatment modalities for complex chronic therapies, further contributing to the field. Combining visionary thinking and industry expertise, Ashar consistently delivers exceptional results and advances specialty drug therapy delivery, always with a patient-first approach.",
+    ],
+  },
+  {
+    name: "SOMAIRA PUNJWANI",
+    title: "Chief Medical Officer & Co-Founder",
+    image: "/assets/team-member.jpg",
+    degree: "PharmD",
+    position: "Chief Medical Officer & Co-Founder",
+    description:
+      "Somaira Punjwani, co-founder of Medmonk, brings over 15 years of pharmacy experience to the organization. Holding a Doctorate in Pharmaceutical Sciences, she has dedicated...",
+    descriptionParagraphs: [
+      "Transitioned from Chief Business Development Officer to Chief Executive Officer, bringing over a decade of experience in strategic innovation and business growth at Medmonk. Passionate about revolutionizing access and affordability, Ashar leads Medmonk, a technology-driven company specializing in patient support services for the treatment of complex chronic rare diseases. As a dedicated pharmacist, Ashar consistently places patients first, ensuring that all strategies and initiatives enhance both patient and provider experiences.",
+      "As the visionary founder and CEO of Premier Specialty Infusion (PSI), Ashar transformed the specialty infusion services landscape nationwide. Previously, co-founded and led Envoy Innovations, empowering healthcare providers with cutting-edge technology to analyze treatment outcomes. At Walgreens/OptionCare, led the Immunoglobulin Program to national prominence following the acquisition of Rx Solutions, where Ashar served as CEO/President and guided it into becoming the fastest-growing specialty injectable provider. Early leadership roles at Crescent Healthcare and Apria Healthcare provided a strong foundation in operational and strategic expertise.",
+      "In addition to leadership roles, Ashar has authored numerous publications related to treatment modalities for complex chronic therapies, further contributing to the field. Combining visionary thinking and industry expertise, Ashar consistently delivers exceptional results and advances specialty drug therapy delivery, always with a patient-first approach.",
+    ],
+  },
+  {
+    name: "BRYAN LI",
+    title: "Chief Technology Officer",
+    image: "/assets/team-member.jpg",
+    degree: "BASc",
+    position: "Chief Technology Officer",
+    description:
+      "Bryan Li, Chief Technology Officer at Medmonk, brings a wealth of experience from leading technology companies to his role, where he spearheads Medmonk’s technology initiatives...",
+    descriptionParagraphs: [
+      "Transitioned from Chief Business Development Officer to Chief Executive Officer, bringing over a decade of experience in strategic innovation and business growth at Medmonk. Passionate about revolutionizing access and affordability, Ashar leads Medmonk, a technology-driven company specializing in patient support services for the treatment of complex chronic rare diseases. As a dedicated pharmacist, Ashar consistently places patients first, ensuring that all strategies and initiatives enhance both patient and provider experiences.",
+      "As the visionary founder and CEO of Premier Specialty Infusion (PSI), Ashar transformed the specialty infusion services landscape nationwide. Previously, co-founded and led Envoy Innovations, empowering healthcare providers with cutting-edge technology to analyze treatment outcomes. At Walgreens/OptionCare, led the Immunoglobulin Program to national prominence following the acquisition of Rx Solutions, where Ashar served as CEO/President and guided it into becoming the fastest-growing specialty injectable provider. Early leadership roles at Crescent Healthcare and Apria Healthcare provided a strong foundation in operational and strategic expertise.",
+      "In addition to leadership roles, Ashar has authored numerous publications related to treatment modalities for complex chronic therapies, further contributing to the field. Combining visionary thinking and industry expertise, Ashar consistently delivers exceptional results and advances specialty drug therapy delivery, always with a patient-first approach.",
+    ],
+  },
+  {
+    name: "COURTNEY SMILEY",
+    title: "Sr. Vice President Business Development",
+    image: "/assets/team-member.jpg",
+    position: "Sr. Vice President Business Development",
+    description:
+      "Courtney spearheads Medmonks business development initiatives, meticulously tracking industry trends and collaborating with biotechnology and pharmaceutical manufacturers...",
+    descriptionParagraphs: [
+      "Transitioned from Chief Business Development Officer to Chief Executive Officer, bringing over a decade of experience in strategic innovation and business growth at Medmonk. Passionate about revolutionizing access and affordability, Ashar leads Medmonk, a technology-driven company specializing in patient support services for the treatment of complex chronic rare diseases. As a dedicated pharmacist, Ashar consistently places patients first, ensuring that all strategies and initiatives enhance both patient and provider experiences.",
+      "As the visionary founder and CEO of Premier Specialty Infusion (PSI), Ashar transformed the specialty infusion services landscape nationwide. Previously, co-founded and led Envoy Innovations, empowering healthcare providers with cutting-edge technology to analyze treatment outcomes. At Walgreens/OptionCare, led the Immunoglobulin Program to national prominence following the acquisition of Rx Solutions, where Ashar served as CEO/President and guided it into becoming the fastest-growing specialty injectable provider. Early leadership roles at Crescent Healthcare and Apria Healthcare provided a strong foundation in operational and strategic expertise.",
+      "In addition to leadership roles, Ashar has authored numerous publications related to treatment modalities for complex chronic therapies, further contributing to the field. Combining visionary thinking and industry expertise, Ashar consistently delivers exceptional results and advances specialty drug therapy delivery, always with a patient-first approach.",
+    ],
+  },
+  {
+    name: "MARY JERCICH",
+    title: "Director of Sales Support & Patient Services",
+    image: "/assets/team-member.jpg",
+    position: "Director of Sales Support & Patient Services",
+    description:
+      "Mary Jercich brings over a decade of specialized experience in the pharmacy realm, making her an indispensable asset to Medmonks mission. Since starting with...",
+    descriptionParagraphs: [
+      "Transitioned from Chief Business Development Officer to Chief Executive Officer, bringing over a decade of experience in strategic innovation and business growth at Medmonk. Passionate about revolutionizing access and affordability, Ashar leads Medmonk, a technology-driven company specializing in patient support services for the treatment of complex chronic rare diseases. As a dedicated pharmacist, Ashar consistently places patients first, ensuring that all strategies and initiatives enhance both patient and provider experiences.",
+      "As the visionary founder and CEO of Premier Specialty Infusion (PSI), Ashar transformed the specialty infusion services landscape nationwide. Previously, co-founded and led Envoy Innovations, empowering healthcare providers with cutting-edge technology to analyze treatment outcomes. At Walgreens/OptionCare, led the Immunoglobulin Program to national prominence following the acquisition of Rx Solutions, where Ashar served as CEO/President and guided it into becoming the fastest-growing specialty injectable provider. Early leadership roles at Crescent Healthcare and Apria Healthcare provided a strong foundation in operational and strategic expertise.",
+      "In addition to leadership roles, Ashar has authored numerous publications related to treatment modalities for complex chronic therapies, further contributing to the field. Combining visionary thinking and industry expertise, Ashar consistently delivers exceptional results and advances specialty drug therapy delivery, always with a patient-first approach.",
+    ],
+  },
+];
+const journeyPaths:string[]=[
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+"Lorem ipsum is a placeholder text commonly used",
+];
+const aboutUsConstants = {
+  asATeamParagraphs,
+  aboutLeadership,
+  teamMembers,
+  journeyPaths
+};
+export default aboutUsConstants;
