fb59314c6e360b9e6c5eca3858c52708aec67991
vishalsolanki40 <solanki.vishal40@gmail.com>
2024-06-27 22:54:27 +0530

    Bug Fixing-27-06-2024

--- Constants/HomePage/HeaderMenuItems.ts
+++ Constants/HomePage/HeaderMenuItems.ts
@@ -3,7 +3,7 @@ import { HeaderMenuItemType } from "@/Interfaces/Home/HeaderMenuItemType";
 export const HeaderMenuItems: HeaderMenuItemType[] = [
     { text: "Home", href: "/" },
     { text: "About Us", href: "/AboutUs" },
-    { text: "Product", href: "/Product" },
+    { text: "Technology Solutions", href: "/Product" },
     { text: "Services", href: "/Services" },
     { text: "Careers", href: "/Careers" },
     // { text: "NEWS & TRENDS", href: "/News" },
