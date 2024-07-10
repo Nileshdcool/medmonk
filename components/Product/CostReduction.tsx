7a128dbdd19a49d31836c5162228e0a81c8e9969
vishalsolanki40 <solanki.vishal40@gmail.com>
2024-07-11 01:53:07 +0530

    Fixed Bugs

--- components/Product/CostReduction.tsx
+++ components/Product/CostReduction.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import coins from "@/public/assets/coins.jpg";
+import coins from "@/public/assets/cost-reduction-business-concept-finance.jpg";
 import { productPageInfo } from "@/Constants/Product/productPageInfo";
 const CostReduction = () => {
   return (
