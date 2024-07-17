a48a88444bcee0942ac18642c4517ebee96da48c
vishalsolanki40 <solanki.vishal40@gmail.com>
2024-07-17 23:18:33 +0530

    Bug-Fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,9 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "home-hero":"url(/assets/home-hero.jpg)",
+          "home-hero":"url(/assets/Home-Hero-Banner.jpeg)",
           "tech-hero":"url(/assets/tech-hero-min.png)",
-          "service-hero":"url(/assets/service-banner.jpg)",
+          "service-hero":"url(/assets/Services-Hero-Banner.jpeg)",
           "get-started":"url(/assets/get-started-img.jpg)",
           "contact-hero":"url(/assets/contact-banner.jpg)",
       },
